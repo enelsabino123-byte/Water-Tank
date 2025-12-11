0d29ad04117ac5cb67715c0417bd7d4e7a321d78
Sabino, Marienell T. <enelsabino123@gmail.com>
2025-12-12 03:57:52 +0800

    Go Home

--- backend/app.js
+++ backend/app.js
@@ -2,9 +2,9 @@ import express from 'express';
 import { dbConnection } from './config/db_access.js';
 import { config as dotenvConfig } from 'dotenv';
 import fs from 'fs';
-
+import deviceRouter from './routers/device.routers.js';
 const app = express();
-
+app.use(express.json());
 const PORT = process.env.PORT || 5000;
 
 const secretPath =
@@ -14,13 +14,12 @@ const secretPath =
 
 dotenvConfig({ path: secretPath});
 
-
 app.get("/", (req, res) =>{
     res.json({message: "Server is working!"})
 });
+app.use("/devices", deviceRouter);
 
 app.listen(PORT, ()=>{
     dbConnection();
     console.log("Server started at http://localhost:"+PORT);
 });
-
